01115a8c3fbf97de4bb1817e02a6854e06639a86
Robin Eschbach <LADBukkit@gmail.com>
2020-04-26 13:43:08 +0200

    added database companion

--- database/src/main/java/de/hsworms/flashcard/database/FCDatabase.kt
+++ database/src/main/java/de/hsworms/flashcard/database/FCDatabase.kt
@@ -1,6 +1,8 @@
 package de.hsworms.flashcard.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import de.hsworms.flashcard.database.dao.FlashcardDao
 import de.hsworms.flashcard.database.entity.Flashcard
@@ -9,4 +11,18 @@ import de.hsworms.flashcard.database.entity.FlashcardNormal
 @Database(entities = [Flashcard::class, FlashcardNormal::class], version = 1)
 abstract class FCDatabase : RoomDatabase() {
     abstract fun flashcardDao(): FlashcardDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: FCDatabase? = null
+
+        fun getDatabase(context: Context): FCDatabase {
+            if(INSTANCE == null){
+                synchronized(this) {
+                    INSTANCE = Room.databaseBuilder(context.applicationContext, FCDatabase::class.java, "flashcard_database").build()
+                }
+            }
+            return INSTANCE!!
+        }
+    }
 }
